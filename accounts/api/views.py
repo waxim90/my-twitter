ed0aa7a52cb03762bfe017d7f5b4d23d7b2eda7a
Xiaomeng Wang <4kwangxiaomeng@gmail.com>
2021-04-23 22:52:22 -0400

    authentication-api

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
-from accounts.api.serializers import UserSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from accounts.api.serializers import (
+    UserSerializer,
+    LoginSerializer,
+    SignupSerializer,
+)
+from django.contrib.auth import (
+    logout as django_logout,
+    login as django_login,
+    authenticate as django_authenticate,
+)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,3 +22,65 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class AccountViewSet(viewsets.ViewSet):
+    serializer_class = SignupSerializer
+
+    @action(methods=['GET'], detail=False)
+    def login_status(self, request):
+        data = {
+            'has_logged_in': request.user.is_authenticated
+        }
+        if request.user.is_authenticated:
+            data['user'] = UserSerializer(request.user).data
+            # data['user'].pop('id')
+        return Response(data)
+
+    @action(methods=['POST'], detail=False)
+    def logout(self, request):
+        django_logout(request)
+        return Response({'Success': True})
+
+    @action(methods=['POST'], detail=False)
+    def login(self, request):
+        # get username and passport from request
+        serializer = LoginSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Please check input.",
+                "errors": serializer.errors,
+            }, status=400)
+        username = serializer.validated_data['username'].lower()
+        password = serializer.validated_data['password'].lower()
+
+        user = django_authenticate(username=username, password=password)
+        if not user or user.is_anonymous:
+            return Response({
+                "success": False,
+                "message": "Username and password does not match.",
+            }, status=400)
+
+        django_login(request, user)
+        return Response({
+            "success": True,
+            "user": UserSerializer(user).data,
+        })
+
+    @action(methods=['POST'], detail=False)
+    def signup(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Please check input.",
+                "errors": serializer.errors,
+            }, status=400)
+
+        user = serializer.save()
+        django_login(request, user)
+        return Response({
+            "success": True,
+            "user": UserSerializer(user).data,
+        }, status=201)
