16fb3332761c9cc915ab0d5a50a91f93053ea792
Xiaomeng Wang <4kwangxiaomeng@gmail.com>
2021-04-23 22:52:22 -0400

    authentication-api

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -2,7 +2,12 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email']
+        fields = ['id', 'username', 'email']
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
