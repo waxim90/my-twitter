6612dafb5cdba330ba80d33b465c110590c71079
Xiaomeng Wang <4kwangxiaomeng@gmail.com>
2021-06-21 21:38:22 -0400

    add TweetPhoto model and admin

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,4 +1,6 @@
 from testing.testcases import TestCase
+from tweets.constants import TweetPhotoStatus
+from tweets.models import TweetPhoto
 from utils.time_helpers import utc_now
 from datetime import timedelta
 
@@ -23,3 +25,13 @@ class TweetTests(TestCase):
         dongxie = self.create_user('dongxie')
         self.create_like(dongxie, self.tweet)
         self.assertEqual(self.tweet.like_set.count(), 2)
+
+    def test_create_photo(self):
+        # 测试可以成功创建 photo 的数据对象
+        photo = TweetPhoto.objects.create(
+            tweet=self.tweet,
+            user=self.linghu,
+        )
+        self.assertEqual(photo.user, self.linghu)
+        self.assertEqual(photo.status, TweetPhotoStatus.PENDING)
+        self.assertEqual(self.tweet.tweetphoto_set.count(), 1)
