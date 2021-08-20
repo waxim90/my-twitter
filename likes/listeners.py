558367d97615cdd064b8482fbef5102ba81f9b24
Xiaomeng Wang <4kwangxiaomeng@gmail.com>
2021-08-20 11:34:14 -0400

    denormalization model change

---
+++ likes/listeners.py
@@ -0,0 +1,56 @@
+from utils.listeners import invalidate_object_cache
+
+
+def incr_likes_count(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    from tweets.models import Tweet
+    from django.db.models import F
+
+    model_class = instance.content_type.model_class()
+    if model_class != Tweet:
+        # TODO HOMEWORK 给 Comment 使用类似的方法进行 likes_count 的统计
+        return
+
+    # tweet = instance.content_object
+    # 不可以使用 tweet.likes_count += 1; tweet.save() 的方式
+    # 因此这个操作不是原子操作，必须使用 F函数 语句才是原子操作
+
+    # F函数用法：
+    # SQL Query: UPDATE likes_count = likes_count + 1 FROM tweets_table WHERE id=<instance.object_id>
+    # 方法1：
+    Tweet.objects.filter(id=instance.object_id)\
+        .update(likes_count=F('likes_count') + 1)
+    invalidate_object_cache(sender=Tweet, instance=instance.content_object)
+
+    # 方法2：
+    # tweet = instance.content_object
+    # tweet.likes_count = F('likes_count') + 1
+    # tweet.save()
+
+
+def decr_likes_count(sender, instance, **kwargs):
+    from tweets.models import Tweet
+    from django.db.models import F
+
+    model_class = instance.content_type.model_class()
+    if model_class != Tweet:
+        # TODO HOMEWORK 给 Comment 使用类似的方法进行 likes_count 的统计
+        return
+
+    # tweet = instance.content_object
+    # 不可以使用 tweet.likes_count -= 1; tweet.save() 的方式
+    # 因此这个操作不是原子操作，必须使用 F函数 语句才是原子操作
+
+    # F函数用法：
+    # SQL Query: UPDATE likes_count = likes_count - 1 FROM tweets_table WHERE id=<instance.object_id>
+    # 方法1：
+    Tweet.objects.filter(id=instance.object_id)\
+        .update(likes_count=F('likes_count') - 1)
+    invalidate_object_cache(sender=Tweet, instance=instance.content_object)
+
+    # 方法2：
+    # tweet = instance.content_object
+    # tweet.likes_count = F('likes_count') - 1
+    # tweet.save()
--- likes/models.py
+++ likes/models.py
@@ -2,8 +2,10 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models.signals import pre_delete, post_save
 
 from utils.memcached_helpers import MemcachedHelper
+from likes.listeners import incr_likes_count, decr_likes_count
 
 
 class Like(models.Model):
@@ -41,3 +43,8 @@ class Like(models.Model):
     @property
     def cached_user(self):
         return MemcachedHelper.get_object_through_cache(User, self.user_id)
+
+
+pre_delete.connect(decr_likes_count,sender=Like)
+post_save.connect(incr_likes_count,sender=Like)
+    
